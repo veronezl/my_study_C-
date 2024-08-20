a37456d653d67daa7d86d571a0f366dcf3b3c393
veronezl <lucasveronez.dev@gmail.com>
2024-08-20 16:03:01 -0300

    add: class IfElse

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using my_study_Csharp.models;
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        // Exemplo de uso da classe Pessoa
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.Nome = "João";
+        pessoa1.Idade = 25;
+        pessoa1.Apresentar();
 
-Pessoa pessoa1 = new Pessoa();
-pessoa1.Nome = "João";
-pessoa1.Idade = 25;
-pessoa1.Apresentar();
+        Console.WriteLine(pessoa1.Nome);
+        Console.WriteLine(pessoa1.Idade);
 
-Console.WriteLine(pessoa1.Nome);
-Console.WriteLine(pessoa1.Idade);
-
-DateTime dataAtual = DateTime.Now;
-Console.WriteLine(dataAtual);
+        DateTime dataAtual = DateTime.Now;
+        Console.WriteLine(dataAtual);
+    }
+}
