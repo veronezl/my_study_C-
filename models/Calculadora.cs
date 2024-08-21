98bc6cfcea2cbb707b200d446ee12005c2613248
veronezl <lucasveronez.dev@gmail.com>
2024-08-21 16:55:53 -0300

    add: class Calculadora

---
+++ models/Calculadora.cs
@@ -0,0 +1,25 @@
+namespace my_study_Csharp.models
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($" {x} / {y} = {x / y}");
+        }
+    }
+}
